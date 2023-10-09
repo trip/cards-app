fdff8f1e6909abb7c592682961e33710fcb5382b
trip <daxton416@gmail.com>
2023-10-09 18:25:56 -0400

    updated clothing-card version number

--- src/cards-app.js
+++ src/cards-app.js
@@ -61,9 +61,9 @@ class CardsApp extends LitElement {
   render() {
     return html`
       <main>
-      <meme-maker alt="Cat stalking a small toy" image-url="https://cdn2.thecatapi.com/images/9j5.jpg" top-text="I bring you" bottom-text="the death">
-      </meme-maker>
-      <clothing-card clothing="Testing t-shirt"></clothing-card>
+        <meme-maker alt="Cat stalking a small toy" image-url="https://cdn2.thecatapi.com/images/9j5.jpg" top-text="I bring you" bottom-text="the death"></meme-maker>
+        <clothing-card clothing="Testing t-shirt" information="Get these now!"></clothing-card>
+    </main>
     `;
   }
 }
